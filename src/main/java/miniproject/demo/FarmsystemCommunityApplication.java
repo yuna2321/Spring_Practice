a201f18d08afb6f953f1a42c13cf4209670b1d4e
yuna2321 <yuna1021@dgu.ac.kr>
2025-08-12 15:33:30 +0900

    setting - 파일 세팅

---
+++ src/main/java/miniproject/demo/FarmsystemCommunityApplication.java
@@ -0,0 +1,13 @@
+package miniproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmsystemCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmsystemCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miniproject/demo/FarmsystemCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmsystemCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
